de894e69a33655873d806a13f0cd6580a406e29b
IanO12 <77741695+IanO12@users.noreply.github.com>
2023-03-29 20:36:54 -0400

    Update game.js

--- Lab/phaserGame/game.js
+++ Lab/phaserGame/game.js
@@ -26,33 +26,33 @@ var config = {
     player = this.add.sprite(400, 300, 'player');
     player.setScale(2);
   
-//     // Set the bounds manually
-//     player.setInteractive();
-//     this.input.on('gameobjectdown', function (pointer, gameObject) {
-//       gameObject.input.draggable = true;
-//       gameObject.input.dragFromCenter = false;
-//       gameObject.input.boundsRect = new Phaser.Geom.Rectangle(0, 0, game.config.width, game.config.height);
-//     });
+     // Set the bounds manually
+     player.setInteractive();
+     this.input.on('gameobjectdown', function (pointer, gameObject) {
+       gameObject.input.draggable = true;
+       gameObject.input.dragFromCenter = false;
+       gameObject.input.boundsRect = new Phaser.Geom.Rectangle(0, 0, game.config.width, game.config.height);
+     });
     
-//     this.input.keyboard.on('keydown_LEFT', function (event) {
-//       player.setVelocityX(-160);
-//     });
+     this.input.keyboard.on('keydown_LEFT', function (event) {
+       player.setVelocityX(-160);
+     });
+ 
+     this.input.keyboard.on('keydown_RIGHT', function (event) {
+       player.setVelocityX(160);
+     });
   
-//     this.input.keyboard.on('keydown_RIGHT', function (event) {
-//       player.setVelocityX(160);
-//     });
+     this.input.keyboard.on('keydown_UP', function (event) {
+       player.setVelocityY(-160);
+     });
+ 
+     this.input.keyboard.on('keydown_DOWN', function (event) {
+       player.setVelocityY(160);
+     });
   
-//     this.input.keyboard.on('keydown_UP', function (event) {
-//       player.setVelocityY(-160);
-//     });
-  
-//     this.input.keyboard.on('keydown_DOWN', function (event) {
-//       player.setVelocityY(160);
-//     });
-  
-//     this.input.keyboard.on('keyup', function (event) {
-//       player.setVelocity(0);
-//     });
+     this.input.keyboard.on('keyup', function (event) {
+       player.setVelocity(0);
+     });
   }
   
   function update() {
